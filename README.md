f0a8d97dd5cea497516223ad7250cb49a128b485
petrelevich <petrelevich@yandex.ru>
2023-12-25 21:05:09 +0300

    L01-gradle

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# otus_java_2023_12
+# Примеры для курса OTUS ["Разработчик Java"](https://otus.ru/lessons/java-professional/?utm_source=github&utm_medium=free&utm_campaign=otus)
+
+
+Группа 2023-12
+
+### Преподаватели
+Сергей Петрелевич<br>
+Стрекалов Павел<br>
+Александр Оруджев<br>
+Вячеслав Лапин<br>
+Виталий Куценко<br>
+Евгений Непомнящий
---
+++ build.gradle.kts
@@ -0,0 +1,90 @@
+import io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension
+import org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+
+plugins {
+    idea
+    id("io.spring.dependency-management")
+    id("org.springframework.boot") apply false
+}
+
+idea {
+    project {
+        languageLevel = IdeaLanguageLevel(21)
+    }
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
+
+
+allprojects {
+    group = "ru.otus"
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+
+    val testcontainersBom: String by project
+    val protobufBom: String by project
+    val guava: String by project
+
+    apply(plugin = "io.spring.dependency-management")
+    dependencyManagement {
+        dependencies {
+            imports {
+                mavenBom(BOM_COORDINATES)
+                mavenBom("org.testcontainers:testcontainers-bom:$testcontainersBom")
+                mavenBom("com.google.protobuf:protobuf-bom:$protobufBom")
+            }
+            dependency("com.google.guava:guava:$guava")
+        }
+    }
+
+    configurations.all {
+        resolutionStrategy {
+            failOnVersionConflict()
+
+            force("javax.servlet:servlet-api:2.4")
+            force("commons-logging:commons-logging:1.1.1")
+            force("commons-lang:commons-lang:2.5")
+            force("org.codehaus.jackson:jackson-core-asl:1.8.8")
+            force("org.codehaus.jackson:jackson-mapper-asl:1.8.3")
+            force("org.codehaus.jettison:jettison:1.1")
+            force("net.java.dev.jna:jna:5.8.0")
+            force("com.google.errorprone:error_prone_annotations:2.7.1")
+            force("org.sonarsource.analyzer-commons:sonar-analyzer-commons:2.3.0.1263")
+            force("com.google.code.findbugs:jsr305:3.0.2")
+            force("org.sonarsource.sslr:sslr-core:1.24.0.633")
+            force("org.eclipse.platform:org.eclipse.osgi:3.18.400")
+            force("org.eclipse.platform:org.eclipse.equinox.common:3.18.0")
+        }
+    }
+}
+
+subprojects {
+    plugins.apply(JavaPlugin::class.java)
+    extensions.configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.compilerArgs.addAll(listOf("-Xlint:all,-serial,-processing"))
+    }
+}
+
+tasks {
+    val managedVersions by registering {
+        doLast {
+            project.extensions.getByType<DependencyManagementExtension>()
+                .managedVersions
+                .toSortedMap()
+                .map { "${it.key}:${it.value}" }
+                .forEach(::println)
+        }
+    }
+}
