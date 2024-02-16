733ea2c55e909c62670b3c72252979f164967585
spv <pvstrekalov@yandex.ru>
2024-02-16 19:21:35 +0400

    L13-creationalPatterns

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,7 @@ include("L09-docker")
 include("L10-byteCodes")
 include("L11-Java8")
 include("L12-solid")
+include("L13-creationalPatterns")
 
 pluginManagement {
     val jgitver: String by settings
